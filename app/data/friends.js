5c49264d801f82e464eec6f62c2e7c06a45379b1
John Hambry <johnphambry@gmail.com>
2019-09-03 14:06:00 -0500

    final

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friendsArray = [
+    {
+      friendName: "Dennis",
+      friendImg: "https://vignette.wikia.nocookie.net/itsalwayssunny/images/5/51/Dennis_%283%29.jpg/revision/latest/scale-to-width-down/180?cb=20110812033321",
+      scores: [
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1,
+          1
+      ]
+    },
+    {
+      friendName: "Mac",
+      friendImg: "https://vignette.wikia.nocookie.net/itsalwayssunny/images/9/9b/Mac_%283%29.jpg/revision/latest/scale-to-width-down/180?cb=20171127055735",
+      scores: [
+          3,
+          1,
+          5,
+          1,
+          2,
+          5,
+          4,
+          1,
+          3,
+          1
+      ]
+    },
+    {
+        friendName: "Charlie",
+      friendImg: "https://vignette.wikia.nocookie.net/itsalwayssunny/images/7/78/Charlie_Kelly.jpg/revision/latest/scale-to-width-down/620?cb=20171214035211",
+      scores: [
+          5,
+          5,
+          5,
+          5,
+          4,
+          4,
+          4,
+          4,
+          1,
+          1
+      ]
+    }
+  ];
+  
+  // Note how we export the array. This makes it accessible to other files using require.
+  module.exports = friendsArray;
